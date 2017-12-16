ad7bb306814e6a6a84821c2a8565aaf35e2fdf0f
Chris Diggs <chris.diggs@e-sainc.com>
2017-12-16 09:13:55 -0600

    Fixed a bug

--- Assets/TCPTestClient.cs
+++ Assets/TCPTestClient.cs
@@ -1,5 +1,4 @@
-﻿
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
@@ -14,6 +13,11 @@ public class TCPTestClient : MonoBehaviour {
 	private Thread clientReceiveThread; 	
 	#endregion  	
 	private IEnumerator coroutine;
+	private string ip   = "10.10.46.147";
+	private int    port = 4352;
+	private string nmea = "$GPGGA";
+	//http://gpsworld.com/what-exactly-is-gps-nmea-data/
+
 
 	// Use this for initialization 	
 	void Start () {
@@ -30,53 +34,63 @@ public class TCPTestClient : MonoBehaviour {
 	/// </summary> 	
 	private void ConnectToTcpServer () { 		
 		print("Starting " + Time.time);
-		coroutine = ListenForData(1.0f);
+		coroutine = ListenForData(3.0f);
 		StartCoroutine(coroutine);
 	}  	
 	/// <summary> 	
-	/// Runs in background clientReceiveThread; Listens for incomming data. 	
+	/// Listen for Data
 	/// </summary>     
-	private IEnumerator ListenForData(float waitTime) { 		
+	private IEnumerator ListenForData(float waitTime) { 	
 		while (true) {
-			
 			try { 			
-				socketConnection = new TcpClient ("10.10.46.52", 4352);  			
-				Byte[] bytes = new Byte[1024];             
-				using (NetworkStream stream = socketConnection.GetStream ()) { 					
-					int length = stream.Read (bytes, 0, bytes.Length);
+				socketConnection = new TcpClient (ip, port);  			
+				Byte[] bytes = new Byte[1024]; 
+
+				while (true) {
+					using (NetworkStream stream = socketConnection.GetStream ()) { 					
+						int length = stream.Read (bytes, 0, bytes.Length);
 						var incommingData = new byte[length]; 						
 						Array.Copy (bytes, 0, incommingData, 0, length); 						
 						string serverMessage = Encoding.ASCII.GetString (incommingData); 	
 
-						Debug.Log(serverMessage);
-
 						string[] srvrMsgs = serverMessage.Split (',');
 
-						if (srvrMsgs [0] == "$GPGGA") {
+						if (srvrMsgs [0] == nmea) {
 							Debug.Log (serverMessage);
 
 							double Lat = Convert.ToDouble (srvrMsgs [2]);
-							double Long1 = Convert.ToDouble (srvrMsgs [4]);
+							double Lon = Convert.ToDouble (srvrMsgs [4]);
 
 							if (srvrMsgs [4] == "S") {
 								Lat = Lat * -1;
 							}
 
 							if (srvrMsgs [4] == "W") {
-								Long1 = Long1 * -1;
+								Lon = Lon * -1;
 							}
 
-							gameObject.GetComponent<TextMesh>().text = "Lat: " + Lat.ToString() + "\nLon: " + Long1.ToString();
-					
-					} 				
+							//Debug.Log ("Ping 1");
+							gameObject.GetComponent<TextMesh> ().text = "Lat: " + Lat.ToString () + "\nLon: " + Lon.ToString ();
+							//Debug.Log ("Ping 2");
+							break;
+						
+						} 	
+					}
 				} 	       
 			} catch (SocketException socketException) {             
 				Debug.Log ("Socket exception: " + socketException);         
 			}  
-
+			//Debug.Log ("Ping 4");
 			yield return new WaitForSeconds (waitTime);
-			print ("Executed " + Time.time);
 		}
 	}
+	
+
+	/*private IEnumerator waitt(float waitTime) {
+		print ("Started waiting " + Time.time);
+		yield return new 
+		print ("Finished Execution " + Time.time);
+	}*/
 
 }
+
